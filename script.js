08ac547c676dcb9c4cca3f5355bb870d0e5e55cd
ffishstix <ffishstix.69@gmail.com>
2024-08-05 22:20:40 +0100

    push for father

--- script.js
+++
@@ -1,8 +0,0 @@
-function pricing(){
-    document.getElementById("myDropdown").classList.toggle("show");
-    const button = document.getElementsByClassName("myDropdown");
-    const default_pricing = "std";
-    button.textContent = default_pricing;
-    button.style.display = "block";
-
-}   
